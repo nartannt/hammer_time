4309f287ba2f7665d48de80c561c585ed2e6904e
nartan <bozec.tanguy@gmail.com>
2026-05-05 17:59:13 +0200

    idk why the environment is provided under a monad

--- case_study/lean/CaseStudy/Examples/MissingLemmas.lean
+++ case_study/lean/CaseStudy/Examples/MissingLemmas.lean
@@ -36,6 +36,7 @@ elab "list_constants" : tactic => do
     let env ← Lean.MonadEnv.getEnv -- get the local environment
     let constants := env.constants -- get the local constants.
     let mut max := 0
+    let mut indCtors : List Lean.Expr := []
     for cnst in constants do
       --let cnstExpr := cnst.toExpr
       max := max + 0
@@ -50,6 +51,7 @@ elab "list_constants" : tactic => do
         && !cnstInfo.name.toString.contains "Hammer" 
         && !cnstInfo.name.toString.contains "LLVM" 
         && !cnstInfo.name.toString.contains "BitVec" 
+        && !cnstInfo.name.toString.contains "IO" 
         && !cnstInfo.name.toString.contains "Lean" then 
         match cnstInfo with
           | Lean.ConstantInfo.inductInfo inductVal => 
@@ -58,10 +60,32 @@ elab "list_constants" : tactic => do
               match (Lean.Environment.find? env ctor) with
                 | some val => (Lean.ConstantInfo.toConstantVal val).type
                 | none => cnstInfo.type)
+            indCtors := ctorsTypes ++ indCtors
             dbg_trace f!"+ local cnst: {cnstName} with constructors: {ctorsTypes}"
           | _ => return
       if max > 10000 then
         return
+
+      return 
+
+def inductive_definitions : List Lean.Expr :=
+    let constants := Lean.Environment.constants >> Lean.MonadEnv.getEnv
+    let rec cnstToIndDefs  (defsAcc : List Lean.Expr) (remainingCnsts : List (Int × Lean.ConstantInfo)) : (List Lean.Expr) :=
+      match remainingCnsts with
+        | [] => defsAcc
+        | (_, cnstInfo) :: cnstsRest =>
+          match cnstInfo with 
+            | Lean.ConstantInfo.inductInfo inductVal => 
+              let ctors := inductVal.ctors
+              let ctorsTypes := (fun env ↦ 
+                (List.filterMap (fun ctor ↦
+                  match Lean.Environment.find? env ctor with
+                    | some val => some (Lean.ConstantInfo.toConstantVal val).type
+                    | none => none) ctors ) ) >> Lean.MonadEnv.getEnv
+              cnstToIndDefs (ctorsTypes ++ defsAcc) cnstsRest
+            | _ => (cnstToIndDefs defsAcc cnstsRest)
+
+    cnstToIndDefs [] >> constants
 
 example : forall x: A, B x := by
   --have h':(forall x: A, B x) := by apply B.b
