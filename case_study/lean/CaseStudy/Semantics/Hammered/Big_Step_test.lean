8f6805982715c3280eb7cb15ee91b71d118ef9cf
Xavier Genereux <xaviergenereux@hotmail.com>
2026-05-11 18:27:17 +0200

    exit slow file

--- case_study/lean/CaseStudy/Semantics/Hammered/Big_Step_test.lean
+++ case_study/lean/CaseStudy/Semantics/Hammered/Big_Step_test.lean
@@ -5,26 +5,26 @@ set_option trace.auto.tptp.printQuery true
 set_option trace.auto.tptp.result true
 
 example (s: State) : (("x" ::= (AExp.num 5));; ("y" ::= (AExp.var "x")), s) ==> s["x" ↦ 5]["y" ↦ 5] := by
-  try hammer [BigStep.seq, BigStep.assign]
+  -- try hammer [BigStep.seq, BigStep.assign]
   apply BigStep.seq <;> apply BigStep.assign
 
 example (s: State) (t: State) (b: BExp) : (((IF b THEN Com.skip ELSE Com.skip), s) ==> t) -> t = s := by
-  hammer
+  -- hammer
   sorry
 
-theorem inversion_skip (s: State) (t: State) : ((SKIP, s) ==> t) -> t = s := by 
-    --try hammer
+theorem inversion_skip (s: State) (t: State) : ((SKIP, s) ==> t) -> t = s := by
+    --try -- hammer
     intro h; cases h; rfl
-theorem inversion_asign (x: String) (a: AExp) (s: State) (t: State) : ((Com.assign x a, s) ==> t) -> t = s[x ↦ (aeval a s)] 
+theorem inversion_asign (x: String) (a: AExp) (s: State) (t: State) : ((Com.assign x a, s) ==> t) -> t = s[x ↦ (aeval a s)]
   := by
     intro h
-    try hammer
-    hammer
+    try -- hammer
+    -- hammer
     cases h
     rfl
-theorem inversion_seq (s: State) (t: State) (c1: Com) (c2: Com) : 
+theorem inversion_seq (s: State) (t: State) (c1: Com) (c2: Com) :
     ((c1;;c2, s) ==> t) -> (exists s', ((c1, s)==> s') ∧ ((c2, s') ==> t)) := by sorry
-theorem inversion_ite (cond: BExp) (ci: Com) (ce: Com) (s: State) (t: State) : 
+theorem inversion_ite (cond: BExp) (ci: Com) (ce: Com) (s: State) (t: State) :
     ((IF cond THEN ci ELSE ce, s) ==> t) -> (( (beval cond s) ∧ ((ci, s) ==> t))
                                             ∨(¬(beval cond s) ∧ ((ce, s) ==> t)) ) := by
                       intros h
@@ -34,11 +34,11 @@ theorem inversion_ite (cond: BExp) (ci: Com) (ce: Com) (s: State) (t: State) :
                           trivial
                         }
                         | if_false cond _ _ _ _ hcond hbody => {right; trivial}
-theorem inversion_while (cond: BExp) (cl: Com) (s: State) (t: State) : 
-    ((WHILE cond DO cl, s) ==> t) -> (  (exists s', (beval cond s) ∧ ((cl, s) ==> s') ∧ ((WHILE cond DO cl, s') ==> t) ) 
+theorem inversion_while (cond: BExp) (cl: Com) (s: State) (t: State) :
+    ((WHILE cond DO cl, s) ==> t) -> (  (exists s', (beval cond s) ∧ ((cl, s) ==> s') ∧ ((WHILE cond DO cl, s') ==> t) )
                                      ∨ (¬(beval cond s) ∧ (t = s)) ) := by sorry
 
-theorem inversion_bs (c: Com) (s: State) (t: State) : ((c, s) ==> t) -> 
+theorem inversion_bs (c: Com) (s: State) (t: State) : ((c, s) ==> t) ->
    (c = SKIP) ∧ (t = s)
   ∨ (exists x a, (c = (Com.assign x a)) ∧ (t = s[x ↦ (aeval a s)]))
   ∨ (exists c1 c2 s', (c = c1;;c2) ∧ ((c1, s) ==> s') ∧ ((c2, s') ==> t))
@@ -50,47 +50,50 @@ theorem inversion_bs (c: Com) (s: State) (t: State) : ((c, s) ==> t) ->
     cases h with
       | skip => { aesop }
       | assign => { aesop }
-      | seq c1 c2 _ s' _ hl hr => { hammer }
-      | if_true cond ci ce _ _ hcond hbody => { hammer }
-      | if_false cond ci ce _ _ hcond hbody => { hammer }
-      | while_true cond cl _ s' _ hcond hbody hrest => { hammer }
-      | while_false cond cl _ s' => { hammer }
+      | seq c1 c2 _ s' _ hl hr => { sorry }
+      | if_true cond ci ce _ _ hcond hbody => { sorry }
+      | if_false cond ci ce _ _ hcond hbody => { sorry }
+      | while_true cond cl _ s' _ hcond hbody hrest => { sorry }
+      | while_false cond cl _ s' => { sorry }
 
 
 theorem ite_skip_2 (s: State) (t: State) (b: BExp) : (((IF b THEN Com.skip ELSE Com.skip), s) ==> t) -> t = s := by
-  hammer [inversion_skip, inversion_ite] {disableAesop := true}
+  --hammer [inversion_skip, inversion_ite] {disableAesop := true}
+  sorry
 
 example (x: String) (a: AExp) (s: State) (s': State) :  (((x ::= a), s) ==> s' ) <-> (s' = s[x ↦ (aeval a s)]) := by
-  constructor
-  { hammer [inversion_assign] {disableAesop := true} }
-  { hammer [inversion_assign] {disableAesop := true} }
+  sorry
+  -- constructor
+  -- { hammer [inversion_assign] {disableAesop := true} }
+  -- { hammer [inversion_assign] {disableAesop := true} }
 
 theorem assign_sim (x: String) (a: AExp) (s: State) (s': State) :  (((x ::= a), s) ==> s' ) <-> (s' = s[x ↦ (aeval a s)]) := by
   constructor
   {
-    hammer
+    sorry
+    -- hammer
     --hammer [inversion_bs] {disableAesop := true}
 
   }
   {
-    try hammer [BigStep.assign] {disableAesop := true}
+    -- try hammer [BigStep.assign] {disableAesop := true}
     intro h
     rw [h]
     apply BigStep.assign
 
   }
-
+#exit
 theorem seq_assoc : (((c1;; c2);; c3, s) ==> s') <-> ((c1;; (c2;; c3), s) ==> s') := by
   constructor
   {
     intro h
-    hammer
+    -- hammer
     sorry
   }
   {
     intro h
     cases h with | seq _ _ _ s1 _ h1 ht =>
-    cases ht with | seq _ _ _ s2 _ h2 h3 => 
+    cases ht with | seq _ _ _ s2 _ h2 h3 =>
     have hi: (((c1;;c2), s) ==> s2) := by
       apply BigStep.seq <;>
       assumption
@@ -113,7 +116,7 @@ example (c: Com) (b: BExp) : ((WHILE b DO c) ~ (IF b THEN c;; WHILE b DO c ELSE 
     cases h' with
     | if_true _ _ _ _ _ _ hb =>
         --cases hb; apply BigStep.while_true <;> assumption
-        hammer
+        -- hammer
         hammer [BigStep.while_true]
     | if_false _ _ _ _ _ _ hb =>
         -- have heq : (s = t) := by hammer; sorry
@@ -128,8 +131,8 @@ theorem triv_if (c: Com) (b: BExp): ((IF b THEN c ELSE c) ~ c) := by
   hammer [inversion_ite, BigStep.if_true, BigStep.if_false]
   sorry
 
-theorem commute_if: (IF b1 THEN (IF b2 THEN c11 ELSE c12) ELSE c2) 
-   ~ 
+theorem commute_if: (IF b1 THEN (IF b2 THEN c11 ELSE c12) ELSE c2)
+   ~
    (IF b2 THEN (IF b1 THEN c11 ELSE c2) ELSE (IF b1 THEN c12 ELSE c2)) := by
   hammer [BigStep.if_true, BigStep.if_false]
   repeat sorry
@@ -163,10 +166,10 @@ theorem sim_while_cong: (c ~ c') -> (WHILE b DO c) ~ (WHILE b DO c') := by
 theorem sim_refl:  ( c ~ c ) := by
   hammer
 
-theorem sim_sym:   ((c ~ c') <-> (c' ~ c)) := by 
+theorem sim_sym:   ((c ~ c') <-> (c' ~ c)) := by
   hammer
 
-theorem sim_trans: ( (c ~ c') -> (c' ~ c'') -> (c ~ c'') ) := by 
+theorem sim_trans: ( (c ~ c') -> (c' ~ c'') -> (c ~ c'') ) := by
   hammer
 
 theorem big_step_determ: (((c,s) ==> t) ∧ ((c,s) ==> u) ) -> (u = t) := by
@@ -175,24 +178,23 @@ theorem big_step_determ: (((c,s) ==> t) ∧ ((c,s) ==> u) ) -> (u = t) := by
   generalize rn : (c, s) = p
   rw [rn] at h_0
   induction h_0 generalizing s u c with
-    | skip => cases rn; hammer
-    | assign => cases rn; hammer
-    | seq _ _ _ t' _ _ _ ih ih' => 
+    | skip => cases rn; -- hammer
+    | assign => cases rn; -- hammer
+    | seq _ _ _ t' _ _ _ ih ih' =>
       cases rn
       --hammer -- cannot run tactic because it returns an error and the file doesn't compile
       sorry
     | if_true _ _ _ _ _ _ _ ih =>
       cases rn
-      hammer
+      -- hammer
       sorry
     | if_false cond ci ce s' t' hcond hobdy ih =>
       cases rn
-      hammer
-      sorry
-    | while_true _ _ _ t' _ _ _ _ ih ih' => 
-      cases rn
       -- hammer
+      sorry
+    | while_true _ _ _ t' _ _ _ _ ih ih' =>
+      cases rn
+      -- -- hammer
       sorry
     | while_false =>
       cases rn; hammer
-
