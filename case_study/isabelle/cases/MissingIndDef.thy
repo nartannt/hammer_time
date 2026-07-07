3e1caedaebb3116316401c8abb9e1f1e6bf584d2
nartan <bozec.tanguy@gmail.com>
2026-07-07 14:36:01 +0200

    cases/MissingIndDef.thy

--- case_study/isabelle/cases/MissingIndDef.thy
+++ case_study/isabelle/cases/MissingIndDef.thy
@@ -5,11 +5,11 @@ begin
 sledgehammer_params [overlord, provers = zipperposition,
                      max_facts = 999 (* HC_MAX_FACTS *), verbose]
 
-(* In the initial example A was a generic type, but it seems this doesn't exist in Isabelle*)
-inductive B :: "Nat \<Rightarrow> Prop" where
-  b: B n for n
+(* In the initial example A was a generic type, but it seems this doesn't exist in Isabelle
+   similarly, prop doesn't seem to exist*)
+inductive test :: "nat \<Rightarrow> bool" for n where
+  constructor: "test n"
 
 lemma lean_hammer_failure:
-  shows "\<forall> x. B x"
+  shows "\<forall> x. test x"
   sledgehammer
-end
